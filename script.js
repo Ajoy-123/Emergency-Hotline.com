9669a7ece5fd0ee6f5972f736d014c86002c598a
Ajoy-123 <70084493+Ajoy-123@users.noreply.github.com>
2025-08-28 17:19:34 +0600

    call to action added

--- script.js
+++ script.js
@@ -14,4 +14,47 @@ for(const heartIcon of heartIcons){
 })
 }
 
+let coins = 100;
+const callButtons = document.querySelectorAll(".call-btn");
+const callHistoryContainer = getId("call-history-container")
+const copyCount = getId("copy-count").innerText
+
+function updateCoins(){
+    getId("coin-balance").innerText = coins;
+}
+  
+    for(const button of callButtons){
+      button.addEventListener("click", function(e){
+        // e.preventDefault();
+        
+        const card = button.closest(".card");
+        const serviceName = card.querySelector(".service-name").innerText;
+        const serviceNumber = card.querySelector(".service-number").innerText;
+        if(coins < 20){
+            alert("Not enough coins! You want at least 20 coins to make a call");
+            return
+        }
+
+        coins -= 20;
+        updateCoins();
+
+        alert(`Calling ${serviceName} at ${serviceNumber}`);
+
+        // get current Time
+
+        const callHistoryContainer = getId("call-history-container");
+        getId("call-history-container").innerText = "";
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+
+        `
+
+    })
+} 
+
+
+
+
+
 
