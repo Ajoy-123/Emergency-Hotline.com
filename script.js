be05026ca1126ccdeea3ee017d05d14d7325b30f
Ajoy-123 <70084493+Ajoy-123@users.noreply.github.com>
2025-08-28 13:15:10 +0600

    heart count completed

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log("Hello")
+// function:: get Id
+function getId(id){
+    return document.getElementById(id);
+}
+
+// heart count
+const heartIcons = document.querySelectorAll(".heart-icon");
+for(const heartIcon of heartIcons){
+    heartIcon.addEventListener("click", function(){
+    let heartCount = Number(getId("heart-count").innerText);
+    heartCount++;
+    getId("heart-count").innerText = heartCount;
+    console.log(heartCount);
+})
+}
+
 
